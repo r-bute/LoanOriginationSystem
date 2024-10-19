416b2ea0272b5d83f6c4bd92f1e1bad882a048b4
ronald_b <68206329+r-bute@users.noreply.github.com>
2024-10-19 00:10:08 -0400

    first commit after spring initializer

---
+++ src/main/java/com/example/loanorigination/LoanoriginationApplication.java
@@ -0,0 +1,13 @@
+package com.example.loanorigination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanoriginationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanoriginationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/loanorigination/LoanoriginationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loanorigination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanoriginationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
